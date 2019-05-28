f8aa7a6f9ca427700812cce072bb67f9a157a760
Matt <matt@michambers.com>
2019-05-28 15:35:21 +0100

    Hooking up MongoDB
    Using Mongoose setting up MongoDB and delete functionality.

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require ('./models/post');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://matt:y2bYRJd07K4743dq@aaf1-8abp3.mongodb.net/AAF?retryWrites=true')
+.then(() => {
+  console.log('Connected to database!');
+})
+.catch(() => {
+  console.log('Connection failed!');
+});
+
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) =>{
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,29 +31,32 @@ app.use((req, res, next) =>{
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: '123',
-      title: 'first server-side post',
-      content: 'this is coming from the server'
-    },
-    {
-      id: '456',
-      title: 'second server-side post',
-      content: 'this is coming from the server'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
+  Post.find().then(documents => {
+    console.log(documents);
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts: documents
+    });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({ message: 'post deleted'});
   });
 });
 
