8498a6a823b54ca18eccfb1793a04594665bcf5f
Matt <matt@michambers.com>
2019-06-07 11:59:27 +0100

    Started implementing signup/login and JWTs

---
+++ backend/routes/user.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+  .then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    user.save()
+    .then(result => {
+      res.status(201).json({
+        message: 'User created',
+        result: result
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+  });
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({
+    email: req.body.email
+  })
+  .then(user => {
+    if (!user) {
+      return res.status(401).json({
+        message: 'Authentication failed'
+      });
+    }
+    return bcrypt.compare(req.body.password, user.password);
+  })
+  .then(result => {
+    if (!result) {
+      return res.status(401).json({
+        message: "Authentication failed"
+      });
+    }
+    const token = jwt.sign({
+      email: user.email,
+      userId: user._id
+    },
+    "secret_this_should_be_longer_and_better",  //jwt secret
+    {expiresIn: "1h"}
+    );
+  })
+  .catch(err => {
+    return res.status(401).json({
+      message: "Authentication failed"
+    });
+  });
+});
+
+module.exports = router;
