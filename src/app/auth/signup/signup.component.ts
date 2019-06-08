a8766bfc646211027ae5525725e16e49fa557d46
Matt <matt@michambers.com>
2019-06-08 21:49:30 +0100

    Added UI features loading spinners

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -10,12 +11,14 @@ import { AuthService } from '../auth.service';
 export class SignupComponent {
   isLoading = false;
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router) {}
 
   onSignup(form: NgForm) {
     if (form.invalid) {
       return;
     }
+    this.isLoading = true;
     this.authService.createUser(form.value.email, form.value.password);
+    this.router.navigate(['/']);
   }
 }
