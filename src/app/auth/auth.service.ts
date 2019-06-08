9aaee4b26da83515dc24d2b5020bb8b697764dbb
Matt <matt@michambers.com>
2019-06-08 18:06:29 +0100

    Auth cont.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,13 @@ import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string){
     const authData: AuthData = {
@@ -23,9 +29,10 @@ export class AuthService {
       email: email,
       password: password
     };
-    this.http.post("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
     .subscribe(response => {
-      
+      const token = response.token;
+      this.token = token;
     });
   }
 }
