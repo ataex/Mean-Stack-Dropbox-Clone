f7c8bbcfea628bb559f30a98eead7215b561a571
Matt <matt@michambers.com>
2019-06-08 21:37:57 +0100

    Hide/Show header items depending on authorization

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private isAuthenticated = false;
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getisAuth() {
+    return this.isAuthenticated;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string){
@@ -33,6 +44,16 @@ export class AuthService {
     .subscribe(response => {
       const token = response.token;
       this.token = token;
+      if (token) {
+        this.isAuthenticated = true;
+        this.authStatusListener.next(true);
+      }
     });
+  }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
   }
 }
