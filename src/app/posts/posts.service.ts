dce9c9f3ff871b71d9743132d0f1aa5c390992be
Matt <matt@michambers.com>
2019-06-10 15:21:26 +0100

    Server side authorization for edit and delete messages

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -25,7 +25,8 @@ export class PostsService {
                 title: post.title,
                 content: post.content,
                 id: post._id,
-                imagePath: post.imagePath
+                imagePath: post.imagePath,
+                creator: post.creator
               };
             }),
             maxPosts: postData.maxPosts
@@ -33,6 +34,7 @@ export class PostsService {
         })
       )
       .subscribe(transformedPostData => {
+        console.log(transformedPostData);
         this.posts = transformedPostData.posts;
         this.postsUpdated.next({
           posts: [...this.posts],
