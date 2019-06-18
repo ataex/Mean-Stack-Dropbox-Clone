884f9245582060e407e3ea93d27b05431037efc5
Matt <matt@michambers.com>
2019-06-19 00:07:50 +0100

    fileNames being returned as tile text

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -22,7 +22,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   isLoading = false;
   totalPosts = 0;
-  postsPerPage = 2;
+  postsPerPage = 4;
   pageSizeOptions = [1, 2, 5, 10];
   currentPage = 1;
   userIsAuthenticated = false;
@@ -49,6 +49,9 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.isLoading = false;
         this.totalPosts = postData.postCount;
         this.posts = postData.posts;
+        for (let i = 0; i < this.tiles.length; i++) {
+          this.tiles[i].text = this.posts[i].fileName;
+        }
       });
     this.userIsAuthenticated = this.authService.getisAuth();
     this.authStatusSub = this.authService
