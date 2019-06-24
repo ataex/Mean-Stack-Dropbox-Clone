092ac7317962aa018eaa0f40b063d0c7cd77c8fe
Matt <matt@michambers.com>
2019-06-24 12:07:00 +0100

    files locked for check in only by user who has checked it out

--- src/app/posts/post-list/file-edit.component.ts
+++ src/app/posts/post-list/file-edit.component.ts
@@ -1,15 +1,16 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { DialogData } from './post-list.component';
-import { windowWhen } from 'rxjs/operators';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'file-edit-component',
   templateUrl: 'file-edit.component.html',
   styleUrls: ['file-edit.component.css']
 })
-export class FileEditDialogComponent implements OnInit {
+export class FileEditDialogComponent implements OnInit, OnDestroy {
 
   public fileId: string;
   public fileDelete = true;
@@ -17,14 +18,26 @@ export class FileEditDialogComponent implements OnInit {
   public fileCheckIn = 'checkIn';
   public downloadFilePath: string;
   public fileName: string;
+  userIsAuthenticated = false;
+  userId: string;
+  private authStatusSub: Subscription;
 
   constructor(
     public dialogRef: MatDialogRef<FileEditDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private authService: AuthService) {}
 
     ngOnInit() {
       this.fileId = this.data.fileId;
       this.downloadFilePath = this.data.filePath;
+      this.userId = this.authService.getUserId();
+      this.userIsAuthenticated = this.authService.getisAuth();
+      this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+        this.userId = this.authService.getUserId();
+      });
     }
 
   onCancelClick() {
@@ -33,6 +46,10 @@ export class FileEditDialogComponent implements OnInit {
 
   onEditClick(fileId) {
     this.dialogRef.close();
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 
 }
